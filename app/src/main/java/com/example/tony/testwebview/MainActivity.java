cbcec5c1f5e1735117e56c6d5a296cc6e5a34450
tonymeng66 <tonymeng66@hotmail.com>
2016-06-13 15:57:45 +0800

    fea:add animation listener

--- app/src/main/java/com/example/tony/testwebview/MainActivity.java
+++ app/src/main/java/com/example/tony/testwebview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tony.testwebview;
 
+import android.annotation.TargetApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,14 +8,130 @@ import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.tony.testwebview.tools.Constants;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mBookWebview;
+    private ImageView mDivider1;
+    private ImageView mDivider2;
+    private Button mButton1;
+    private Button mButton2;
+    View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button1:
+                    mBookWebview.startAnimation(mPatTwoToThreeAni);
+                    mDivider1.startAnimation(mPatTwoToThreeAni);
+                    mDivider2.startAnimation(mPatTwoToThreeAni);
+                    break;
+                case R.id.button2:
+                    mBookWebview.startAnimation(mPatThreeToTwoAni);
+                    mDivider1.startAnimation(mPatThreeToTwoAni);
+                    mDivider2.startAnimation(mPatThreeToTwoAni);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
 
     private Animation mPatOneToTwoAni;
     private Animation mPatTwoToOneAni;
     private Animation mPatTwoToThreeAni;
     private Animation mPatThreeToTwoAni;
+
+    Animation.AnimationListener mPatTwoToThreeAniListener = new Animation.AnimationListener()
+    {
+        public void onAnimationEnd(Animation paramAnimation)
+        {
+//            HelpActivity.this.listBookLevelOneName.setVisibility(0);
+//            int i;
+//            if (HelpActivity.this.bookLists.size() > HelpActivity.this.lBookOneSelectedPosition)
+//            {
+//                i = HelpActivity.this.lBookOneSelectedPosition;
+//                if (HelpActivity.this.mCurType != 1)
+//                    break label248;
+//                HelpActivity.this.listRootName.setText(HelpActivity.this.getString(2131427342), HelpActivity.this.getResources().getColor(2131230944));
+//                HelpActivity.this.listBookLevelOneName.setText(((HelpList)HelpActivity.this.bookLists.get(i)).getIndexName(), HelpActivity.this.getResources().getColor(2131230943));
+//            }
+//            while (true)
+//            {
+                mDivider1.setLayoutParams(mPatThreeDeviderOneParams);
+                mDivider2.setLayoutParams(mPatThreeDeviderTwoParams);
+
+//                HelpActivity.this.listViewBookOne.setVisibility(4);
+//                HelpActivity.this.listViewBookTwo.requestFocus();
+//                HelpActivity.this.listViewBookTwo.setLayoutParams(HelpActivity.this.mPatThreeLevelTwoParams);
+                mBookWebview.setVisibility(View.VISIBLE);
+                mBookWebview.setLayoutParams(mPatThreeWebConParams);
+//                HelpActivity.this.loadUrlWithoutFocus(HelpActivity.this.listViewBookTwo.getSelectedItemPosition());
+//                return;
+//                i = 0;
+//                break;
+//                label248: HelpActivity.this.listRootName.setText(HelpActivity.this.getString(2131427341), HelpActivity.this.getResources().getColor(2131230944));
+//                HelpActivity.this.listBookLevelOneName.setText(((HelpList)HelpActivity.this.listsOriginal.get(i)).getIndexName(), HelpActivity.this.getResources().getColor(2131230943));
+//            }
+        }
+
+        public void onAnimationRepeat(Animation paramAnimation)
+        {
+        }
+
+        public void onAnimationStart(Animation paramAnimation)
+        {
+        }
+    };
+    Animation.AnimationListener mPatThreeToTwoAniListener = new Animation.AnimationListener()
+    {
+        public void onAnimationEnd(Animation paramAnimation)
+        {
+            mDivider1.setLayoutParams(mPatTwoDeviderOneParams);
+//            HelpActivity.this.listViewBookOne.setVisibility(0);
+//            HelpActivity.this.listViewBookOne.requestFocus();
+//            HelpActivity.this.listViewBookOne.setLayoutParams(HelpActivity.this.mPatTwoLevelOneParams);
+            mDivider2.setLayoutParams(mPatTwoDeviderTwoParams);
+//            HelpActivity.this.listViewBookTwo.setLayoutParams(HelpActivity.this.mPatTwoLevelTwoParams);
+            //mBookWebview.setVisibility(View.INVISIBLE);
+            mBookWebview.setLayoutParams(mPatTwoWebConParams);
+//            HelpActivity.this.mWebViewScrollbarBg.setVisibility(4);
+//            HelpActivity.this.mWebViewScrollbarWhiteThumb.setVisibility(4);
+        }
+
+        public void onAnimationRepeat(Animation paramAnimation)
+        {
+        }
+
+        public void onAnimationStart(Animation paramAnimation)
+        {
+        }
+    };
+
+
+    private RelativeLayout.LayoutParams mPatOneDeviderOneParams;
+    private RelativeLayout.LayoutParams mPatOneDeviderTwoParams;
+    private RelativeLayout.LayoutParams mPatOneLevelOneParams;
+    private RelativeLayout.LayoutParams mPatOneLevelRootParams;
+    private RelativeLayout.LayoutParams mPatOnewLevelTwoParams;
+    private RelativeLayout.LayoutParams mPatThreeDeviderOneParams;
+    private RelativeLayout.LayoutParams mPatThreeDeviderTwoParams;
+    private RelativeLayout.LayoutParams mPatThreeLevelOneParams;
+    private RelativeLayout.LayoutParams mPatThreeLevelRootParams;
+    private RelativeLayout.LayoutParams mPatThreeLevelTwoParams;
+    private RelativeLayout.LayoutParams mPatThreeWebConParams;
+    private RelativeLayout.LayoutParams mPatTwoDeviderOneParams;
+    private RelativeLayout.LayoutParams mPatTwoDeviderTwoParams;
+    private RelativeLayout.LayoutParams mPatTwoLevelOneParams;
+    private RelativeLayout.LayoutParams mPatTwoLevelRootParams;
+    private RelativeLayout.LayoutParams mPatTwoLevelTwoParams;
+    private RelativeLayout.LayoutParams mPatTwoWebConParams;
+
+
 
 
     @Override
@@ -23,17 +140,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initAnimation();
-        final WebView bookWebview = (WebView) findViewById(R.id.bookWebview);
-        bookWebview.loadUrl("file:///android_asset/55PUF6850/Connect_your_TV/Connect_antenna/description.html");
-        Button button1 = (Button)findViewById(R.id.botton1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                bookWebview.startAnimation(mPatOneToTwoAni);
-            }
-        });
-
+        initComponetLp();
+        findViews();
     }
+
+    private void findViews()
+    {
+        mBookWebview = (WebView)findViewById(R.id.bookWebview);
+        mBookWebview.loadUrl("file:///android_asset/55PUF6850/Connect_your_TV/Connect_antenna/description.html");
+        mDivider1 = (ImageView)findViewById(R.id.bookDividerOne);
+        mDivider2 = (ImageView)findViewById(R.id.bookDividerTwo);
+        mButton1 = (Button)findViewById(R.id.button1);
+        mButton1.setOnClickListener(buttonClickListener);
+        mButton2 = (Button)findViewById(R.id.button2);
+        mButton2.setOnClickListener(buttonClickListener);
+    }
+
 
     private void initAnimation()
     {
@@ -45,9 +167,40 @@ public class MainActivity extends AppCompatActivity {
         //this.mPatTwoToOneAni.setAnimationListener(this.mPatTwoToOneAniListener);
         this.mPatTwoToThreeAni = new TranslateAnimation(0.0F, -532.0F, 0.0F, 0.0F);
         this.mPatTwoToThreeAni.setDuration(500L);
-        //this.mPatTwoToThreeAni.setAnimationListener(this.mPatTwoToThreeAniListener);
+        this.mPatTwoToThreeAni.setAnimationListener(mPatTwoToThreeAniListener);
         this.mPatThreeToTwoAni = new TranslateAnimation(0.0F, 532.0F, 0.0F, 0.0F);
         this.mPatThreeToTwoAni.setDuration(500L);
-        //this.mPatThreeToTwoAni.setAnimationListener(this.mPatThreeToTwoAniListener);
+        this.mPatThreeToTwoAni.setAnimationListener(mPatThreeToTwoAniListener);
     }
+
+    private void initComponetLp()
+    {
+//        this.mPatOneDeviderTwoParams = new RelativeLayout.LayoutParams(-2, -2);
+//        this.mPatOneDeviderTwoParams.leftMargin = 520;
+//        this.mPatOneLevelOneParams = new RelativeLayout.LayoutParams(Constants.lvOneWidth, Constants.lvDisplayHeight);
+//        this.mPatOneLevelOneParams.leftMargin = 538;
+
+
+        this.mPatTwoDeviderOneParams = new RelativeLayout.LayoutParams(-1, -1);
+        this.mPatTwoDeviderOneParams.leftMargin = 108;
+//        this.mPatTwoLevelOneParams = new RelativeLayout.LayoutParams(370, Constants.lvDisplayHeight);
+//        this.mPatTwoLevelOneParams.leftMargin = 124;
+        this.mPatTwoDeviderTwoParams = new RelativeLayout.LayoutParams(-1, -1);
+        this.mPatTwoDeviderTwoParams.leftMargin = 606;
+//        this.mPatTwoLevelTwoParams = new RelativeLayout.LayoutParams(316, Constants.lvDisplayHeight);
+//        this.mPatTwoLevelTwoParams.leftMargin = 624;
+        this.mPatTwoWebConParams = new RelativeLayout.LayoutParams(1134, 640);
+        this.mPatTwoWebConParams.leftMargin = 1226;
+
+
+        this.mPatThreeDeviderOneParams = new RelativeLayout.LayoutParams(-1, -1);
+        this.mPatThreeDeviderOneParams.leftMargin = 646;
+//        this.mPatThreeLevelTwoParams = new RelativeLayout.LayoutParams(316, Constants.lvDisplayHeight);
+//        this.mPatThreeLevelTwoParams.leftMargin = 192;
+        this.mPatThreeDeviderTwoParams = new RelativeLayout.LayoutParams(-1, -1);
+        this.mPatThreeDeviderTwoParams.leftMargin = 604;
+        this.mPatThreeWebConParams = new RelativeLayout.LayoutParams(1134, 640);
+        this.mPatThreeWebConParams.leftMargin = 506;
+    }
+
 }
---
+++ app/src/main/java/com/example/tony/testwebview/tools/Constants.java
@@ -0,0 +1,73 @@
+package com.example.tony.testwebview.tools;
+
+public class Constants
+{
+  private static final float PX_PER_DP = 2.0F;
+  public static int bookWebViewIndex;
+  public static int gridViewIndex;
+  public static int gvFirstPageLastPosition;
+  public static int gvScrollToPosition;
+  public static int gvSecondPageFirstPosition;
+  public static int gvSecondPageLastPosition;
+  public static int indexItemWidth;
+  public static int indexPageNumber;
+  public static int lvAndWvBookTwoTopMargin;
+  public static int lvBookOneIndex;
+  public static int lvBookOneRightMargin;
+  public static int lvBookTwoIndex;
+  public static int lvBookTwoLeftMargin;
+  public static int lvBookTwoTopMargin;
+  public static int lvDisplayHeight;
+  public static int lvDisplayLeftMargin;
+  public static int lvHideHeight;
+  public static int lvHideWidth;
+  public static int lvLastPosition;
+  public static int lvListOneIndex = 100;
+  public static int lvOneWidth;
+  public static int lvPageNumber;
+  public static int lvScrollByDistance;
+  public static int lvScrollDuration;
+  public static int lvTwoWidth;
+  public static int onePageNumber;
+  public static int twoPageNumber;
+  public static int webViewScrollBarTop;
+  public static int webViewWhiteBgTop;
+  public static int wvScrollByDistance;
+
+  static
+  {
+    lvBookOneIndex = 101;
+    lvBookTwoIndex = 102;
+    gridViewIndex = 103;
+    indexPageNumber = 14;
+    indexItemWidth = 50;
+    lvPageNumber = 9;
+    lvLastPosition = 8;
+    lvHideWidth = 500;
+    lvHideHeight = 702;
+    lvOneWidth = 540;
+    lvDisplayHeight = 720;
+    lvTwoWidth = 472;
+    lvDisplayLeftMargin = 50;
+    gvFirstPageLastPosition = 13;
+    gvSecondPageFirstPosition = 14;
+    gvScrollToPosition = 1400;
+    webViewWhiteBgTop = 0;
+    webViewScrollBarTop = 0;
+    lvScrollByDistance = 58;
+    lvScrollDuration = 0;
+    lvBookTwoLeftMargin = 140;
+    lvBookTwoTopMargin = 0;
+    lvBookOneRightMargin = 660;
+    lvAndWvBookTwoTopMargin = 740;
+    wvScrollByDistance = 60;
+    bookWebViewIndex = 201;
+    onePageNumber = 14;
+    twoPageNumber = 28;
+  }
+}
+
+/* Location:           D:\apk\ebony2K15_tecent5507_Help_pack_classes_dex2jar.jar
+ * Qualified Name:     com.tpv.xmic.help.ebony2k15.tool.Constants
+ * JD-Core Version:    0.6.0
+ */
